f0c9e85b8f168e4082d8e4868263eea3a4e6823e
btssio <btssio@btssio-VirtualBox>
2014-12-04 17:35:18 +0100

    ajout classes metiers

---
+++ src/modele/metier/M_region.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modele.metier;
+
+/**
+ *
+ * @author btssio
+ */
+public class M_region {
+    
+    
+    private int reg_code;
+    private M_secteur reg_sec;
+    private String reg_nom;
+}
---
+++ src/modele/metier/M_secteur.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modele.metier;
+
+/**
+ *
+ * @author btssio
+ */
+public class M_secteur {
+    
+    private int sec_code;
+    private String sec_libelle;
+
+    public int getSec_code() {
+        return sec_code;
+    }
+
+    public void setSec_code(int sec_code) {
+        this.sec_code = sec_code;
+    }
+
+    public String getSec_libelle() {
+        return sec_libelle;
+    }
+
+    public void setSec_libelle(String sec_libelle) {
+        this.sec_libelle = sec_libelle;
+    }
+    
+    
+    
+    
+}
